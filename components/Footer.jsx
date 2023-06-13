1aeaf5e1135385ac90272755a4cbe77953eb199c
Anjali <anjalirathod222001@gmail.com>
2023-06-13 10:34:31 +0530

    Final Change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>Anju's Fashion Hub All rights reserverd</p>
+      <p>CARA All rights reserverd</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Anju's Fashion Hub</title>
+        <title>CARA</title>
       </Head>
       <header>
         <Navbar />
